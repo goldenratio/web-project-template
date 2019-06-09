c387bf55286677b4efec344005ed87cdc1c3e520
kuuuurija <karthikeyanvj@gmail.com>
2019-06-09 15:00:38 +0300

    added rxjs example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import { interval } from 'rxjs';
 import { sum } from './add';
 import { HelloWorld } from './hello-world';
 
+const ob$ = interval(1000);
+ob$.subscribe(val => {
+  console.log(val);
+});
+
 console.log(new HelloWorld());
 console.log(sum(40, 2));
-
